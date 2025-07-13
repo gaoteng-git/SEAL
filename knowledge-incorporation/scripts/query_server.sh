ac70cd1f8a72b23877b1556a9ba467aa4693c231
Teng Gao <teng@0g.ai>
2025-07-13 09:27:00 +0000

    use best trained model, use best self-edit data to adapter

--- knowledge-incorporation/scripts/query_server.sh
+++ knowledge-incorporation/scripts/query_server.sh
@@ -13,7 +13,7 @@
 SERVER_HOST="127.0.0.1"  # set to TTT server IP
 ZMQ_PORT=5555
 
-OUTPUT_DIR="knowledge-incorporation/results/query_server_trained6"
+OUTPUT_DIR="knowledge-incorporation/results/query_server_trained7"
 mkdir -p "${OUTPUT_DIR}"
 
 # -------- Experiment Grid -------------------------------------------- #
