02ce2e866fc24908f947cc4db75343b747781260
gaoteng <gaoteng@xiaoice.com>
2025-08-17 18:23:54 +0800

    remove end mask

--- general-knowledge/src/inner/TTT_server.py
+++ general-knowledge/src/inner/TTT_server.py
@@ -57,7 +57,7 @@ from ..utils import (
 
 # ---------------------------  CONFIG & LOGGING  ----------------------- #
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(name)s - %(message)s",
     datefmt="%H:%M:%S",
 )
